9941747f016d431ee7c3c2d9c50c5f625aafbc76
mrlim@mrlim.net <mrlim@mrlim.net@52f67a3d-da4c-c82c-9fa6-35572dec3218>
2011-03-13 21:50:47 +0000

    Cleaned up formatting, comments.

--- trunk/XII - Competition/src/edu/wpi/first/wpilibj/templates/PIDLineSource.java
+++ trunk/XII - Competition/src/edu/wpi/first/wpilibj/templates/PIDLineSource.java
@@ -2,9 +2,9 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package edu.wpi.first.wpilibj.templates;
 
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.PIDSource;
 
 /**
@@ -13,15 +13,44 @@ import edu.wpi.first.wpilibj.PIDSource;
  */
 public class PIDLineSource implements PIDSource {
 
+    //Line Followers
+    DigitalInput digLineLeft, digLineMiddle, digLineRight;
     public double lineError = 0;
+    public double prevLineError = 0;
 
-    PIDLineSource()
-    {
-
+    PIDLineSource() {
+        //Line Followers
+        digLineLeft = new DigitalInput(ElectricalMap.kLightSensorLChannel);
+        digLineMiddle = new DigitalInput(ElectricalMap.kLightSensorMChannel);
+        digLineRight = new DigitalInput(ElectricalMap.kLightSensorRChannel);
     }
 
-    public double pidGet()
-    {
+    public double pidGet() {
+        //Read and update line follower error
+        if (digLineLeft.get() && digLineMiddle.get() && digLineRight.get()) {
+            lineError = prevLineError * 2;
+        } else if (!digLineLeft.get() && !digLineMiddle.get() && !digLineRight.get()) {
+            lineError = 0;
+            prevLineError = 0;
+        } else if (!digLineLeft.get() && digLineMiddle.get() && !digLineRight.get()) {
+            lineError = 0;
+            prevLineError = 0;
+        } else if (digLineLeft.get() && !digLineMiddle.get() && digLineRight.get()) {
+            lineError = 0;
+            prevLineError = 0;
+        } else if (!digLineLeft.get() && !digLineMiddle.get() && digLineRight.get()) {
+            lineError = -1;
+            prevLineError = -1;
+        } else if (digLineLeft.get() && !digLineMiddle.get() && !digLineRight.get()) {
+            lineError = 1;
+            prevLineError = 1;
+        } else if (!digLineLeft.get() && digLineMiddle.get() && digLineRight.get()) {
+            lineError = -2;
+            prevLineError = -2;
+        } else if (digLineLeft.get() && digLineMiddle.get() && !digLineRight.get()) {
+            lineError = 2;
+            prevLineError = 2;
+        }
         return lineError;
     }
 }
