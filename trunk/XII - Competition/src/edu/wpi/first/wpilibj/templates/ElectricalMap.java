99b30ed47ddbe50912e8111b8928e711a6e28d04
mrlim@mrlim.net <mrlim@mrlim.net@52f67a3d-da4c-c82c-9fa6-35572dec3218>
2011-03-13 19:52:29 +0000

    Fixed NetBeans Project

--- trunk/XII - Competition/src/edu/wpi/first/wpilibj/templates/ElectricalMap.java
+++ trunk/XII - Competition/src/edu/wpi/first/wpilibj/templates/ElectricalMap.java
@@ -4,63 +4,53 @@
  * electrcial map found at http://projects.zoho.com/portal/dmorrison33 under
  * Wiki - [Electrical] Competition Wiring Map
  */
-
 package edu.wpi.first.wpilibj.templates;
 
 public class ElectricalMap {
-    //A 0 MEANS THAT THE VALUE HAS NOT BEEN SET - 0'S ARE BAD
 
     //Analog Module
-
     public static final int kAnalogModulePort = 1;
-
+    //Gyro
     public static final int kGyroChannel = 1;
-
-    public static final int kAcclerometerChannelA = 2;
-    public static final int kAcclerometerChannelB = 3;
-
+    //Accelerometer
+    public static final int kAccelerometerChannelA = 2;
+    public static final int kAccelerometerChannelB = 3;
+    //Ultrasonic
     public static final int kUltrasonicChannel = 4;
-
     //Solenoid Module
-
     public static final int kSolenoidModulePort = 7;
-
+    //Shifter
     public static final int kSolenoidLowChannel = 2;
     public static final int kSolenoidHighChannel = 1;
-
+    //Minibot Deployment
     public static final int kSolenoidMinibotA = 7;
     public static final int kSolenoidMinibotB = 8;
-
     //Digital Sidecar
-    
     public static final int kDigitalModulePort = 4;
-
+    //Line Follower Light Sensors
     public static final int kLightSensorRChannel = 2;
     public static final int kLightSensorMChannel = 3;
     public static final int kLightSensorLChannel = 4;
-
+    //Gripper Treads
     public static final int kVictorGripperTopChannel = 5;
     public static final int kVictorGripperBottomChannel = 6;
-
+    //Flux Capacitor
     public static final int kRelayFluxOneChannel = 2;
     public static final int kRelayFluxTwoChannel = 3;
-
+    //Compressor
     public static final int kCompressorRelayChannel = 1;
     public static final int kCompressorPressureSwitchChannel = 1;
-
     //CAN Adapters
-
+    //Drivetrain Motors
     //TODO: Reprogram CAN ID of Jag 1 to 10
     public static final int kJaguarRightMaster = 10; //TODO: Comp is 2
     public static final int kJaguarRightSlave = 8; //TODO: Comp is 10
     public static final int kJaguarLeftMaster = 4;
     public static final int kJaguarLeftSlave = 3;
+    //Shoulder Motors
     public static final int kJaguarShoulderOne = 7;
     public static final int kJaguarShoulderTwo = 9; //TODO: Comp is 8
-
     //Driver Station
-    
     public static final int kJoystickDriverPort = 1;
     public static final int kJoystickOperatorPort = 2;
-
 }
