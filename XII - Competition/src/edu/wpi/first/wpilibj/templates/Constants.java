10dc3f660e20585a501dcfa68c01af52d468b306
mrlim <mrlim@mrlim.net>
2011-04-25 16:06:35 +0000

    Partial - Autonomous Constants.

---
+++ XII - Competition/src/edu/wpi/first/wpilibj/templates/Constants.java
@@ -0,0 +1,31 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.wpi.first.wpilibj.templates;
+
+/**
+ *
+ * This class contains a centralized file for various constants.
+ * These constants are primarily pot positions, and autonomous parameters.
+ */
+public class Constants {
+
+    //Analog Module
+    public static final double kGyroFactor = 30;
+    public static final double pArmFrontRaised = 0.414;
+    public static final double pArmBackPickUp = 0.875;
+    public static final double dStartToRack = 10.3;
+    public static final double dRackToTurn = 3.69;
+    public static final double dTurnToTube = 3.31;
+    public static final double dDecel = 2.7;
+    public static final double tExtendWait = 0.5;
+    public static final double tRollTime = 1.35;
+    public static final double tBackUpStart = 2;
+    public static final double tBackUpDuration = 3;
+    public static final double tBackUpArmWait = 3;
+    public static final double sLineFollow = 0.8;
+    public static final double sGyro = 0.8;
+    public static final double sGripperOffset = 0.7;
+    public static final double sFinalBackUp = 0.4;
+}
