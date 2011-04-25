30cf0632adb297f7c0d1e3a410084c34464adbee
patrickwhite@crescentschool.org <patrickwhite@crescentschool.org@52f67a3d-da4c-c82c-9fa6-35572dec3218>
2011-04-25 23:03:55 +0000

    Removed references to second minibot Solenoid.

--- trunk/XII - Competition/src/edu/wpi/first/wpilibj/templates/ElectricalMap.java
+++ trunk/XII - Competition/src/edu/wpi/first/wpilibj/templates/ElectricalMap.java
@@ -24,7 +24,6 @@ public class ElectricalMap {
     public static final int kSolenoidHighChannel = 1;
     //Minibot Deployment
     public static final int kSolenoidMinibotA = 7;
-    public static final int kSolenoidMinibotB = 8;//TODO: NO
     //Gripper Release
     public static final int kSolenoidGripperChannel = 8;//practice robot has none
     //Digital Sidecar
