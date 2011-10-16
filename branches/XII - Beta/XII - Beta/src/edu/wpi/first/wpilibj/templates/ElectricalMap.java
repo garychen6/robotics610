5cb6522fe4f178efc6c0972d41c7caf0487d9831
patrickwhite@crescentschool.org <patrickwhite@crescentschool.org@52f67a3d-da4c-c82c-9fa6-35572dec3218>
2011-10-16 21:58:13 +0000

    Changed Project name to XII - Beta

---
+++ branches/XII - Beta/XII - Beta/src/edu/wpi/first/wpilibj/templates/ElectricalMap.java
@@ -0,0 +1,56 @@
+ /*
+ * This class contains a number of constants of electrical slots and channels
+ * to be referred to instead of in the code - this should directly reflect the
+ * electrcial map found at http://projects.zoho.com/portal/dmorrison33 under
+ * Wiki - [Electrical] Competition Wiring Map
+ */
+package edu.wpi.first.wpilibj.templates;
+
+public class ElectricalMap {
+
+    //Analog Module
+    public static final int kAnalogModulePort = 1;
+    //Gyro
+    public static final int kGyroChannel = 1;
+    //Accelerometer
+    public static final int kAccelerometerChannelA = 2;
+    public static final int kAccelerometerChannelB = 3;
+    //Ultrasonic
+    public static final int kUltrasonicChannel = 4;
+    //Solenoid Module
+    public static final int kSolenoidModulePort = 7;
+    //Shifter
+    public static final int kSolenoidLowChannel = 2;
+    public static final int kSolenoidHighChannel = 1;
+    //Minibot Deployment
+    public static final int kSolenoidMinibotA = 7;
+    //Gripper Release
+    public static final int kSolenoidGripperChannel = 8;//practice robot has none
+    //Digital Sidecar
+    public static final int kDigitalModulePort = 4;
+    //Line Follower Light Sensors
+    public static final int kLightSensorRChannel = 2;//practice robot has none
+    public static final int kLightSensorMChannel = 3;//practice robot has none
+    public static final int kLightSensorLChannel = 4;//practice robot has none
+    //Gripper Treads
+    public static final int kVictorGripperTopChannel = 5;
+    public static final int kVictorGripperBottomChannel = 6;
+    //Flux Capacitor
+    public static final int kRelayFluxOneChannel = 2;//practice robot has none
+    public static final int kRelayFluxTwoChannel = 3;//practice robot has none
+    //Compressor
+    public static final int kCompressorRelayChannel = 1;
+    public static final int kCompressorPressureSwitchChannel = 1;
+    //CAN Adapters
+    //Drivetrain Motors
+    public static final int kJaguarRightMaster = 2;
+    public static final int kJaguarRightSlave = 1;
+    public static final int kJaguarLeftMaster = 4;
+    public static final int kJaguarLeftSlave = 3;
+    //Shoulder Motors
+    public static final int kJaguarShoulderOne = 7;
+    public static final int kJaguarShoulderTwo = 8;//practice robot has none
+    //Driver Station
+    public static final int kJoystickDriverPort = 1;
+    public static final int kJoystickOperatorPort = 2;
+}
