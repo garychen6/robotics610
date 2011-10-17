71e232e76ea955b6c4bfddf70e1578da74bed81b
patrickwhite@crescentschool.org <patrickwhite@crescentschool.org@52f67a3d-da4c-c82c-9fa6-35572dec3218>
2011-10-17 01:02:15 +0000

    Task 3

---
+++ branches/XII - Beta/src/org/crescentschool/robotics/beta/constants/ElectricalBoard.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.crescentschool.robotics.beta.constants;
+
+/**
+ *
+ * @author Patrick
+ */
+public class ElectricalBoard {
+    //Jaguars
+    public static final int kJaguarRightMaster = 2;
+    public static final int kJaguarRightSlave = 1;
+    public static final int kJaguarLeftMaster = 4;
+    public static final int kJaguarLeftSlave = 3;
+    //Compressor
+    public static final int kCompressorRelayChannel = 1;
+    public static final int kCompressorPressureSwitchChannel = 1;
+    //Shifter Solenoids
+    public static final int kSolenoidLowChannel = 2;
+    public static final int kSolenoidHighChannel = 1;
+}
