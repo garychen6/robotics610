cbe5725574ff9a5a1ab2702b08f5cef4f9679350
slim@crescentschool.org <slim@crescentschool.org@52f67a3d-da4c-c82c-9fa6-35572dec3218>
2011-11-15 21:36:21 +0000

    Added Gripper Command

--- branches/XII - Beta/src/org/crescentschool/robotics/beta/subsystems/Gripper.java
+++ branches/XII - Beta/src/org/crescentschool/robotics/beta/subsystems/Gripper.java
@@ -5,22 +5,35 @@
 package org.crescentschool.robotics.beta.subsystems;
 
 import edu.wpi.first.wpilibj.Victor;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import org.crescentschool.robotics.beta.commands.GripperControl;
 import org.crescentschool.robotics.beta.constants.ElectricalConstants;
 
 /**
  *
  * @author slim
  */
-public class Gripper {
+public class Gripper extends Subsystem{
+
+    private static Gripper instance = null;
     private Victor vicGripperTop, vicGripperBottom;
-    
-    private Gripper(){
+
+    public static Gripper getInstance() {
+        if (instance == null) {
+            instance = new Gripper();
+
+            // Set default command here, like this:
+            instance.setDefaultCommand(new GripperControl());
+        }
+        return instance;
+    }
+
+    private Gripper() {
         vicGripperTop = new Victor(ElectricalConstants.kVictorGripperTopChannel);
         vicGripperBottom = new Victor(ElectricalConstants.kVictorGripperBottomChannel);
     }
-    
-    public void runGripper(double topSpeed, double bottomSpeed)
-    {
+
+    public void runGripper(double topSpeed, double bottomSpeed) {
         vicGripperTop.set(topSpeed);
         vicGripperBottom.set(bottomSpeed);
     }
