d465eef496fefd2e94b499d6697f5c02d19de9cd
warfajibril@crescentschool.org <warfajibril@crescentschool.org@52f67a3d-da4c-c82c-9fa6-35572dec3218>
2012-01-11 02:13:44 +0000

    2012 Project Creation

---
+++ trunk/XIII - Competition/src/org/crescentschool/robotics/competition/OI.java
@@ -0,0 +1,7 @@
+
+package org.crescentschool.robotics.competition;
+
+public class OI {
+    // Process operator interface input here.
+}
+
---
+++ trunk/XIII - Competition/src/org/crescentschool/robotics/competition/subsystems/Feeder.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.crescentschool.robotics.competition.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ * @author Warfa
+ */
+public class Feeder extends Subsystem {
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+}
