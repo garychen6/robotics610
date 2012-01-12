5362cfc53cc0cc209c096f4fc1ff6e0f012f928f
warfajibril@crescentschool.org <warfajibril@crescentschool.org@52f67a3d-da4c-c82c-9fa6-35572dec3218>
2012-01-12 23:43:59 +0000

    PID Subsystem

--- trunk/XIII - Competition/src/org/crescentschool/robotics/competition/OI.java
+++ trunk/XIII - Competition/src/org/crescentschool/robotics/competition/OI.java
@@ -1,7 +1,28 @@
 
 package org.crescentschool.robotics.competition;
 
+import edu.wpi.first.wpilibj.Joystick;
+
 public class OI {
-    // Process operator interface input here.
+
+    public static OI instance = null;
+    public static Joystick joyDriver;
+    public static OI getInstance(){
+        
+        
+        if(instance == null){
+            instance = new OI();
+        }
+        return instance;
+    }
+    
+    public Joystick getDriver(){
+        return joyDriver;
+    }
+    
+    private OI(){        
+         joyDriver = new Joystick(1);
+    }
 }
+    
 
