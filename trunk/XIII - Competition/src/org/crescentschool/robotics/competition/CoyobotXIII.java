e7d17f5ad71481c743afa30159a93cbe2a0f1135
warfajibril@crescentschool.org <warfajibril@crescentschool.org@52f67a3d-da4c-c82c-9fa6-35572dec3218>
2012-01-14 14:36:22 +0000

    Added cascading PIDs for DriveTrain

--- trunk/XIII - Competition/src/org/crescentschool/robotics/competition/CoyobotXIII.java
+++ trunk/XIII - Competition/src/org/crescentschool/robotics/competition/CoyobotXIII.java
@@ -4,13 +4,14 @@
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
-
 package org.crescentschool.robotics.competition;
 
-
+import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
+import org.crescentschool.robotics.competition.constants.ElectricalConstants;
+import org.crescentschool.robotics.competition.subsystems.DriveTrain;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -21,8 +22,7 @@ import edu.wpi.first.wpilibj.command.Scheduler;
  */
 public class CoyobotXIII extends IterativeRobot {
 
-    Command autonomousCommand;
-
+   Compressor compressor;
     /**
      * This function is run when the robot is first started up and should be
      * used for any initialization code.
@@ -33,11 +33,15 @@ public class CoyobotXIII extends IterativeRobot {
 
         // Initialize all subsystems
         //CommandBase.init();
+        OI.getInstance();
+        DriveTrain.getInstance();
+        compressor = new Compressor(ElectricalConstants.kCompressorPressureSwitchChannel, ElectricalConstants.kCompressorRelayChannel);
+        compressor.start();
     }
 
     public void autonomousInit() {
         // schedule the autonomous command (example)
-        autonomousCommand.start();
+       
     }
 
     /**
@@ -48,11 +52,11 @@ public class CoyobotXIII extends IterativeRobot {
     }
 
     public void teleopInit() {
-		// This makes sure that the autonomous stops running when
-		// teleop starts running. If you want the autonomous to 
-		// continue until interrupted by another command, remove
-		// this line or comment it out.
-		autonomousCommand.cancel();
+        // This makes sure that the autonomous stops running when
+        // teleop starts running. If you want the autonomous to 
+        // continue until interrupted by another command, remove
+        // this line or comment it out.
+
     }
 
     /**
--- trunk/XIII - Competition/src/org/crescentschool/robotics/competition/constants/PIDConstants.java
+++ trunk/XIII - Competition/src/org/crescentschool/robotics/competition/constants/PIDConstants.java
@@ -9,5 +9,19 @@ package org.crescentschool.robotics.competition.constants;
  * @author Warfa
  */
 public class PIDConstants {
+    public static final boolean pidTuneMode = true;
     
+    public static final double posP = 0;
+    public static final double posI = 0;
+    public static final double posD = 0;
+    public static final double speedP = 0;
+    public static final double speedI = 0.008;
+    public static final double speedD = 0;
+    public static final double gyroP = 0;
+    
+    // Dimensions in inches
+    public static final double wheelCircumference = 4 * Math.PI;
+    
+    // Speeds in feet/second
+    public static final double maxDriveSpeed = 10;
 }
