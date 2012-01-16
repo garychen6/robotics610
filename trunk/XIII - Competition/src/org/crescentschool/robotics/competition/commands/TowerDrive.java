701df949ccea3f230a93f37b753d8c752f7b89d4
warfajibril@crescentschool.org <warfajibril@crescentschool.org@52f67a3d-da4c-c82c-9fa6-35572dec3218>
2012-01-16 22:28:32 +0000

    Tower Drive Update

---
+++ trunk/XIII - Competition/src/org/crescentschool/robotics/competition/commands/TowerDrive.java
@@ -0,0 +1,65 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.crescentschool.robotics.competition.commands;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.command.Command;
+import org.crescentschool.robotics.competition.OI;
+import org.crescentschool.robotics.competition.constants.InputConstants;
+import org.crescentschool.robotics.competition.subsystems.DriveTrain;
+
+/**
+ *
+ * @author Robotics
+ */
+public class TowerDrive extends Command {
+
+    OI oi;
+    DriveTrain driveTrain;
+    int set = 0;
+    boolean dPadUp = false;
+    boolean dPadDown = false;
+    public TowerDrive() {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+        oi = OI.getInstance();
+        driveTrain = DriveTrain.getInstance();
+        requires(driveTrain);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+        if(oi.getDriver().getRawAxis(6) == -1 && !dPadUp){
+            set++;
+            dPadUp = true;
+        }else if(oi.getDriver().getRawAxis(6) == 1 && !dPadDown){
+            set--;
+            dPadDown = true;
+        }
+        if(oi.getDriver().getRawAxis(6) == 0){
+            dPadDown = false;
+            dPadUp = false;
+        }
+        driveTrain.posSetpoint(set);
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
--- trunk/XIII - Competition/src/org/crescentschool/robotics/competition/constants/PIDConstants.java
+++ trunk/XIII - Competition/src/org/crescentschool/robotics/competition/constants/PIDConstants.java
@@ -11,8 +11,8 @@ package org.crescentschool.robotics.competition.constants;
 public class PIDConstants {
     public static final boolean pidTuneMode = true;
     
-    public static final double posP = 500;
-    public static final double posI = 500;
+    public static final double posP = 0;
+    public static final double posI = 0;
     public static final double posD = 0;
     public static final double speedP = -0.0020;
     public static final double speedI = 0.006;
