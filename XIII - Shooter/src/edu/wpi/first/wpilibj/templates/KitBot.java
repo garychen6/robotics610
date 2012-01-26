c23eb64ec40752466a2843204908e40e4ff2bc8f
ianlo <ianlo@crescentschool.org>
2012-01-26 21:10:16 +0000

    Right Triggers accelerates top and bottom. Left triggers slow down.

--- XIII - Shooter/src/edu/wpi/first/wpilibj/templates/KitBot.java
+++ XIII - Shooter/src/edu/wpi/first/wpilibj/templates/KitBot.java
@@ -27,7 +27,11 @@ public class KitBot extends IterativeRobot {
     Shooter shoot = new Shooter();
     Command autonomousCommand;
     double vToM;
-    AnalogChannel ultraSonic;
+    AnalogChannel ultraSonic ;
+    double realRange;
+    int topPower;
+    int bottomPower;
+    boolean buttonPressed = false;
 
     /**
      * This function is run when the robot is first started up and should be
@@ -35,6 +39,7 @@ public class KitBot extends IterativeRobot {
      */
     public void robotInit() {
         // instantiate the command used for the autonomous period
+        ultraSonic = new AnalogChannel(1);
         autonomousCommand = new ExampleCommand();
 
         // Initialize all subsystems
@@ -58,10 +63,12 @@ public class KitBot extends IterativeRobot {
         // teleop starts running. If you want the autonomous to 
         // continue until interrupted by another command, remove
         // this line or comment it out.
-        ultraSonic = new AnalogChannel(1);
+        
         vToM = 0.38582677165354330708661417322835;
         ultraSonic.setAverageBits(4);
-
+        topPower = 700;
+        bottomPower = -1200;
+        
 
     }
 
@@ -74,8 +81,60 @@ public class KitBot extends IterativeRobot {
         
 
         try {
-            System.out.println("top = " + (int) (shoot.topJaguar.getSpeed()) + " goal = " + (int) (130 * ultraSonic.getAverageVoltage() / vToM));
-            System.out.println("bottom = " + (int) (shoot.bottomJaguar.getSpeed()) + " goal = " + (int) (-300 * ultraSonic.getAverageVoltage() / vToM));
+            //shoot.bottomJaguar.setX(OI.joyDriver.getRawAxis(1));
+            if(OI.getInstance().getDriver().getRawButton(6) && !buttonPressed){
+                topPower += 5;
+                buttonPressed = true;
+            }else if(OI.getInstance().getDriver().getRawButton(6) == false){
+                buttonPressed = false;
+            }
+            if(OI.getInstance().getDriver().getRawButton(5) && !buttonPressed){
+                topPower -= 5;
+                buttonPressed = true;
+            }else if(OI.getInstance().getDriver().getRawButton(5) == false){
+                buttonPressed = false;
+            }
+            if(OI.getInstance().getDriver().getRawButton(8) && !buttonPressed){
+                bottomPower -=5;
+                buttonPressed = true;
+            }else if(OI.getInstance().getDriver().getRawButton(8) == false){
+                buttonPressed = false;
+            }
+            if(OI.getInstance().getDriver().getRawButton(7) && !buttonPressed){
+                bottomPower +=5;
+                buttonPressed = true;
+            }else if(OI.getInstance().getDriver().getRawButton(7)){
+                buttonPressed = false;
+            }
+            
+            if (OI.getInstance().getDriver().getRawButton(4) == true) {
+                
+                
+                //shoot.topJaguar.setX(realRange*180);
+                //shoot.bottomJaguar.setX(realRange*-350);
+                //System.out.println("Button Pressed " + (int)realRange);
+                
+            } else {
+                
+                shoot.topJaguar.setX(topPower);
+                shoot.bottomJaguar.setX(bottomPower);
+                System.out.print("Distance = " + (int) (ultraSonic.getAverageVoltage()/vToM*3.28));
+                System.out.print(" bottom = "+ (int)shoot.bottomJaguar.getSpeed());
+                System.out.println(" top = " + (int)shoot.topJaguar.getSpeed());
+               
+                
+              //  shoot.topJaguar.setX(realRange*180);
+              //  shoot.bottomJaguar.setX(realRange*-350);
+              //  realRange = ultraSonic.getAverageVoltage() / vToM;
+              //  System.out.println(
+              //      "top = " + (int) (shoot.topJaguar.getSpeed()) + " goal = " + (int) (180 * ultraSonic.getAverageVoltage()
+              //      / vToM));
+            // System.out.println(
+              //      "bottom = " + (int) (shoot.bottomJaguar.getSpeed()) + " goal = " + (int) (-350 * ultraSonic.getAverageVoltage()
+              //      / vToM));
+            }
+
+            
         } catch (CANTimeoutException ex) {
             ex.printStackTrace();
         }
