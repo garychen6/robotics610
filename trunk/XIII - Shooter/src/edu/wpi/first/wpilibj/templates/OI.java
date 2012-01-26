434d254662a816fc4ab31e451e916565fd11b75e
ianlo@crescentschool.org <ianlo@crescentschool.org@52f67a3d-da4c-c82c-9fa6-35572dec3218>
2012-01-26 21:10:16 +0000

    Right Triggers accelerates top and bottom. Left triggers slow down.

--- trunk/XIII - Shooter/src/edu/wpi/first/wpilibj/templates/OI.java
+++ trunk/XIII - Shooter/src/edu/wpi/first/wpilibj/templates/OI.java
@@ -10,11 +10,46 @@ import edu.wpi.first.wpilibj.buttons.DigitalIOButton;
  * interface to the commands and command groups that allow control of the robot.
  */
 public class OI {
+
+    public static OI instance = null;
     public static Joystick joyDriver;
+    public static Joystick joyOperator;
     
+
+    public static OI getInstance() {
+
+
+        if (instance == null) {
+            instance = new OI();
+        }
+        return instance;
+    }
+
+    /**
+     * Returns the Joystick object that corresponds to the driver's gamepad.
+     * @return the driver Joystick object.
+     */
     public Joystick getDriver() {
         return joyDriver;
     }
     
-}
+    /**
+     * Returns the Joystick object that corresponds to the operator's gamepad.
+     * @return the operator Joystick object.
+     */
+    public Joystick getOperator(){
+        return joyOperator;
+    }
 
+    /**
+     * 
+     * @return the DriverStationLCD object
+     */
+   
+
+    private OI() {
+        joyDriver = new Joystick(1);
+        joyOperator = new Joystick(2);
+  
+    }
+}
