ff1f97ad125c35daf8487cc8284adef152b2c8ac
ianlo <ianlo@crescentschool.org>
2012-01-30 22:14:08 +0000

    Flipper Preset Buttons Added

--- XIII - Competition/src/org/crescentschool/robotics/competition/commands/FlipperPresets.java
+++ XIII - Competition/src/org/crescentschool/robotics/competition/commands/FlipperPresets.java
@@ -18,6 +18,11 @@ public class FlipperPresets extends Command {
     OI oi = OI.getInstance();
     Flipper flipper = Flipper.getInstance();
     Joystick driver = oi.getDriver();
+    boolean button1 = false;
+    boolean button2 = false;
+    boolean button3 = false;
+    boolean button4 = false;
+    
     public FlipperPresets() {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
@@ -28,10 +33,43 @@ public class FlipperPresets extends Command {
     protected void initialize() {
         
     }
+    
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
       
+      if(oi.getDriver().getRawButton(1) && !button1){
+          flipper.setFlippers(0);
+          button1 = true;
+      }
+      if(!oi.getDriver().getRawButton(1)){
+          button1 = false;
+      }
+      
+      if(oi.getDriver().getRawButton(2)){
+          flipper.setFlippers(15);
+          button1 = true;
+      }
+      if(!oi.getDriver().getRawButton(2)){
+          button2 = false;
+          
+      }
+      
+      if(oi.getDriver().getRawButton(3)){
+          flipper.setFlippers(45);
+          button1 = true;
+      }
+      if(!oi.getDriver().getRawButton(3)){
+          button3 = false;
+      }
+      
+      if(oi.getDriver().getRawButton(4)){
+          flipper.setFlippers(90);
+          button1 = true;
+      }
+      if(!oi.getDriver().getRawButton(4)){
+          button4 = false;
+      }
     }
 
     // Make this return true when this Command no longer needs to run execute()
