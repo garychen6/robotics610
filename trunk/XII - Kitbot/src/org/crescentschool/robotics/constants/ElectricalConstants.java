85b31ac0a184209c6a4581be4561cde37aa8c471
warfajibril@crescentschool.org <warfajibril@crescentschool.org@52f67a3d-da4c-c82c-9fa6-35572dec3218>
2012-02-02 21:15:49 +0000

    gycd,kcdk

--- trunk/XII - Kitbot/src/org/crescentschool/robotics/constants/ElectricalConstants.java
+++ trunk/XII - Kitbot/src/org/crescentschool/robotics/constants/ElectricalConstants.java
@@ -12,4 +12,5 @@ public class ElectricalConstants {
     public static final int kJagShooterBottomID = 6;
     public static final int kJagShooterTopID = 7;
     public static final int kUltasonicChannel = 1;
+    public boolean fail = false;
 }
