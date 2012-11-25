eca3d30e9ca0cd82a8ec7dc5df395ba0f6fcc8c4
ianlo <ianlo@crescentschool.org>
2012-11-25 03:57:06 +0000

    Added comments and setup instructions at the top.

--- PreSeason2012/Save_Depth_To_Variable_Roborealm/src/save_depth_to_variable_roborealm/Save_Depth_To_Variable_Roborealm.java
+++ PreSeason2012/Save_Depth_To_Variable_Roborealm/src/save_depth_to_variable_roborealm/Save_Depth_To_Variable_Roborealm.java
@@ -1,12 +1,24 @@
+/* 
+ * Remember to turn off RGB and Infrared,
+ * Set to raw depth instead of grayscale,
+ * Set min threshhold to 0.
+ */
+
 int width = 640;
 int height = 480;
-unsigned char *pixels = getPixels(&width, &height);
 
+unsigned char *pixels = getPixels(&width, &height);
+//Take the 12 red bits.
 unsigned int red = pixels[width*height*3/2-width*3/2-1];
+
+//Take the 4 green bits.
 unsigned int green = pixels[width*height*3/2-width*3/2+1];
 green<<=8;
+//Add them together.
 float total = green+red;
+//Magic Equation.
 total = 1.0 / (total * -0.0030711016 + 3.3309495161);
+//Save it to available variables as a float.
 setFloatVariable("TOTAL", total);
 
 printf("Total = %f \n", total);
