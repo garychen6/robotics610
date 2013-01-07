d28c4c74a0aafbe5d2fc71664bacaa9eb6e211b5
ianlo@crescentschool.org <ianlo@crescentschool.org@52f67a3d-da4c-c82c-9fa6-35572dec3218>
2013-01-07 19:31:53 +0000

    Added TeamPDF

--- trunk/Scouting/KSI/src/ksi/HelloPDF.java
+++ trunk/Scouting/KSI/src/ksi/HelloPDF.java
@@ -30,28 +30,43 @@ import java.util.logging.Logger;
 
 public class HelloPDF {
 
-    private static String FILE = "C:/Users/Warfa/Desktop/DATABASE/ScoutingSheet.pdf";
+    private static String matchFile = "C:/temp/Match.pdf";
+    private static String teamFile = "C:/temp/Team.pdf";
     private static Font catFont = new Font(Font.FontFamily.TIMES_ROMAN, 18,
             Font.BOLD);
     private static Font redFont = new Font(Font.FontFamily.TIMES_ROMAN, 18,
             Font.NORMAL, BaseColor.RED);
     private static Font blueFont = new Font(Font.FontFamily.TIMES_ROMAN, 18,
             Font.NORMAL, BaseColor.BLUE);
-    
-    HelloPDF(String FILE) {
-        this.FILE = FILE;
+
+    HelloPDF(String directory) {
+        this.matchFile = directory + "/Match.pdf";
+        this.teamFile = directory + "/Team.pdf";
     }
-    
-    
+
     public static void main(String[] args) {
         try {
 
 
             TeamSheet sheet = new TeamSheet("610");
+            sheet.setAutoStart("L");
             createMatchPDF(sheet, sheet, sheet, sheet, sheet, sheet);
 
+            MatchSheet matchSheet = new MatchSheet(1);
+            matchSheet.setTeamNum("610");
+            matchSheet.setAutonPointsScored(9001);
+            matchSheet.setDefense(10);
+            matchSheet.setHangLevel(3);
+            matchSheet.setHangTime(0.01);
+            matchSheet.setShotsAttemptedHigh(1);
+            matchSheet.setShotsScoredHigh(1);
+            matchSheet.setStartingPos("C");
 
+            MatchSheet[] array = new MatchSheet[2];
+            array[0] = matchSheet;
+            array[1] = matchSheet;
 
+            createTeamPDF(array);
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -63,16 +78,16 @@ public class HelloPDF {
         Document document = new Document();
         try {
             //Make the PDF writer
-            PdfWriter.getInstance(document, new FileOutputStream(FILE));
+            PdfWriter.getInstance(document, new FileOutputStream(matchFile));
         } catch (FileNotFoundException ex) {
         }
-        
+
         document.open();
         //Create a paragraph
         Paragraph content = new Paragraph();
         //Create the table
         PdfPTable table = new PdfPTable(4);
-        
+
         //Add the headers
         PdfPCell c1 = new PdfPCell(new Phrase("Field"));
         c1.setHorizontalAlignment(Element.ALIGN_CENTER);
@@ -95,54 +110,54 @@ public class HelloPDF {
         table.addCell(blue1.getNumMatches() + "");
         table.addCell(blue2.getNumMatches() + "");
         table.addCell(blue3.getNumMatches() + "");
-        
+
         //Add the cells for the starting positions
         table.addCell("Starts Auto");
-        table.addCell(blue1.getAutoStart() +"");
-        table.addCell(blue2.getAutoStart() +"");
-        table.addCell(blue3.getAutoStart() +"");
-        
+        table.addCell(blue1.getAutoStart() + "");
+        table.addCell(blue2.getAutoStart() + "");
+        table.addCell(blue3.getAutoStart() + "");
+
         //Add the cells for auto points
         table.addCell("Auto Pts");
-        table.addCell(blue1.getAutoPoints() +"");
-        table.addCell(blue2.getAutoPoints() +"");
-        table.addCell(blue3.getAutoPoints() +"");
+        table.addCell(blue1.getAutoPoints() + "");
+        table.addCell(blue2.getAutoPoints() + "");
+        table.addCell(blue3.getAutoPoints() + "");
 
         //Add the cells for teleop scoring percentage
         table.addCell("Teleop Scoring %");
-        table.addCell(blue1.getTeleScoringPercentage() +"");
-        table.addCell(blue2.getTeleScoringPercentage() +"");
-        table.addCell(blue3.getTeleScoringPercentage() +"");
+        table.addCell(blue1.getTeleScoringPercentage() + "");
+        table.addCell(blue2.getTeleScoringPercentage() + "");
+        table.addCell(blue3.getTeleScoringPercentage() + "");
 
         //Add the cells for the average points in teleop
         table.addCell("Teleop Avg Pts");
-        table.addCell(blue1.getTelePoints() +"");
-        table.addCell(blue2.getTelePoints() +"");
-        table.addCell(blue3.getTelePoints() +"");
+        table.addCell(blue1.getTelePoints() + "");
+        table.addCell(blue2.getTelePoints() + "");
+        table.addCell(blue3.getTelePoints() + "");
 
         //Add the cells for defensive rating
         table.addCell("Defensive Rating");
-        table.addCell(blue1.getDefenseRating() +"");
-        table.addCell(blue2.getDefenseRating() +"");
-        table.addCell(blue3.getDefenseRating() +"");
+        table.addCell(blue1.getDefenseRating() + "");
+        table.addCell(blue2.getDefenseRating() + "");
+        table.addCell(blue3.getDefenseRating() + "");
 
         //Add the cells for the time required to hang
         table.addCell("Time Hang");
-        table.addCell(blue1.getHangTime() +"");
-        table.addCell(blue2.getHangTime() +"");
-        table.addCell(blue3.getHangTime() +"");
+        table.addCell(blue1.getHangTime() + "");
+        table.addCell(blue2.getHangTime() + "");
+        table.addCell(blue3.getHangTime() + "");
 
         //Add the cells for the level the robot can hang at
         table.addCell("Hang Level");
-        table.addCell(blue1.getHangLevel() +"");
-        table.addCell(blue2.getHangLevel() +"");
-        table.addCell(blue3.getHangLevel() +"");
+        table.addCell(blue1.getHangLevel() + "");
+        table.addCell(blue2.getHangLevel() + "");
+        table.addCell(blue3.getHangLevel() + "");
 
         //Add the cells for the average KPR
         table.addCell("Avg KPR");
-        table.addCell(blue1.getKPR() +"");
-        table.addCell(blue2.getKPR() +"");
-        table.addCell(blue3.getKPR() +"");
+        table.addCell(blue1.getKPR() + "");
+        table.addCell(blue2.getKPR() + "");
+        table.addCell(blue3.getKPR() + "");
 
 
 
@@ -170,54 +185,54 @@ public class HelloPDF {
         table1.addCell(red1.getNumMatches() + "");
         table1.addCell(red2.getNumMatches() + "");
         table1.addCell(red3.getNumMatches() + "");
-        
+
         //Add the cells for the starting positions
         table1.addCell("Starts Auto");
-        table1.addCell(red1.getAutoStart() +"");
-        table1.addCell(red2.getAutoStart() +"");
-        table1.addCell(red3.getAutoStart() +"");
-        
+        table1.addCell(red1.getAutoStart() + "");
+        table1.addCell(red2.getAutoStart() + "");
+        table1.addCell(red3.getAutoStart() + "");
+
         //Add the cells for auto points
         table1.addCell("Auto Pts");
-        table1.addCell(red1.getAutoPoints() +"");
-        table1.addCell(red2.getAutoPoints() +"");
-        table1.addCell(red3.getAutoPoints() +"");
+        table1.addCell(red1.getAutoPoints() + "");
+        table1.addCell(red2.getAutoPoints() + "");
+        table1.addCell(red3.getAutoPoints() + "");
 
         //Add the cells for teleop scoring percentage
         table1.addCell("Teleop Scoring %");
-        table1.addCell(red1.getTeleScoringPercentage() +"");
-        table1.addCell(red2.getTeleScoringPercentage() +"");
-        table1.addCell(red3.getTeleScoringPercentage() +"");
+        table1.addCell(red1.getTeleScoringPercentage() + "");
+        table1.addCell(red2.getTeleScoringPercentage() + "");
+        table1.addCell(red3.getTeleScoringPercentage() + "");
 
         //Add the cells for the average points in teleop
         table1.addCell("Teleop Avg Pts");
-        table1.addCell(red1.getTelePoints() +"");
-        table1.addCell(red2.getTelePoints() +"");
-        table1.addCell(red3.getTelePoints() +"");
+        table1.addCell(red1.getTelePoints() + "");
+        table1.addCell(red2.getTelePoints() + "");
+        table1.addCell(red3.getTelePoints() + "");
 
         //Add the cells for defensive rating
         table1.addCell("Defensive Rating");
-        table1.addCell(red1.getDefenseRating() +"");
-        table1.addCell(red2.getDefenseRating() +"");
-        table1.addCell(red3.getDefenseRating() +"");
+        table1.addCell(red1.getDefenseRating() + "");
+        table1.addCell(red2.getDefenseRating() + "");
+        table1.addCell(red3.getDefenseRating() + "");
 
         //Add the cells for the time required to hang
         table1.addCell("Time Hang");
-        table1.addCell(red1.getHangTime() +"");
-        table1.addCell(red2.getHangTime() +"");
-        table1.addCell(red3.getHangTime() +"");
+        table1.addCell(red1.getHangTime() + "");
+        table1.addCell(red2.getHangTime() + "");
+        table1.addCell(red3.getHangTime() + "");
 
         //Add the cells for the level the robot can hang at
         table1.addCell("Hang Level");
-        table1.addCell(red1.getHangLevel() +"");
-        table1.addCell(red2.getHangLevel() +"");
-        table1.addCell(red3.getHangLevel() +"");
+        table1.addCell(red1.getHangLevel() + "");
+        table1.addCell(red2.getHangLevel() + "");
+        table1.addCell(red3.getHangLevel() + "");
 
         //Add the cells for the average KPR
         table1.addCell("Avg KPR");
-        table1.addCell(red1.getKPR() +"");
-        table1.addCell(red2.getKPR() +"");
-        table1.addCell(red3.getKPR() +"");
+        table1.addCell(red1.getKPR() + "");
+        table1.addCell(red2.getKPR() + "");
+        table1.addCell(red3.getKPR() + "");
 
         Paragraph paragraph = new Paragraph("Blue Alliance", blueFont);
         paragraph.setAlignment(Element.ALIGN_CENTER);
@@ -234,98 +249,129 @@ public class HelloPDF {
         document.close();
     }
 
-    /*
     public static void createTeamPDF(MatchSheet[] matches)
             throws DocumentException {
         Document document = new Document();
         try {
-            PdfWriter.getInstance(document, new FileOutputStream(FILE));
+            PdfWriter.getInstance(document, new FileOutputStream(teamFile));
         } catch (FileNotFoundException ex) {
         }
         document.open();
 
         Paragraph content = new Paragraph();
-        
-        
-        PdfPTable table = new PdfPTable(matches.length + 1);
-        
+
+
+        PdfPTable table = new PdfPTable(matches.length + 2);
+
         PdfPCell c1 = new PdfPCell(new Phrase("Match #"));
         c1.setHorizontalAlignment(Element.ALIGN_CENTER);
-        
+
         table.addCell(c1);
-        
+
         for (int i = 0; i < matches.length; i++) {
-            c1 = new PdfPCell(new Phrase(matches[i].matchNum));
+            c1 = new PdfPCell(new Phrase(matches[i].getMatchNum() + ""));
             c1.setHorizontalAlignment(Element.ALIGN_CENTER);
             table.addCell(c1);
         }
+
+        c1 = new PdfPCell(new Phrase("Average"));
+        c1.setHorizontalAlignment(Element.ALIGN_CENTER);
+        table.addCell(c1);
+
         
         table.addCell("Starting Position");
         for (int i = 0; i < matches.length; i++) {
-            table.addCell(matches[i].getStart);
+            table.addCell(matches[i].getStartingPos());
         }
-        
+        table.addCell("N/A");
+
         table.addCell("Auto Points");
+        double total = 0;
         for (int i = 0; i < matches.length; i++) {
-            table.addCell(matches[i].get);
+            total+=matches[i].getAutonPointsScored();
+            table.addCell(matches[i].getAutonPointsScored() + "");
         }
+        table.addCell(total/matches.length + "");
         
+        total = 0;
         table.addCell("Teleop Low Attempted");
         for (int i = 0; i < matches.length; i++) {
-            table.addCell(matches[i].get);
+            table.addCell(matches[i].getShotsAttemptedLow() + "");
         }
+        table.addCell(total/matches.length + "");
         
+        total = 0;
         table.addCell("Teleop Low Scored");
         for (int i = 0; i < matches.length; i++) {
-            table.addCell(matches[i].get);
+            table.addCell(matches[i].getShotsScoredLow() + "");
         }
-        
+        table.addCell(total/matches.length + "");
+
+        total = 0;
         table.addCell("Teleop Middle Attempted");
         for (int i = 0; i < matches.length; i++) {
-            table.addCell(matches[i].get);
+            table.addCell(matches[i].getShotsAttemptedMiddle() + "");
         }
+        table.addCell(total/matches.length + "");
         
+        
+        total = 0;
         table.addCell("Teleop Middle Scored");
         for (int i = 0; i < matches.length; i++) {
-            table.addCell(matches[i].get);
+            table.addCell(matches[i].getShotsScoredMiddle() + "");
         }
-        
+        table.addCell(total/matches.length + "");
+
+        total = 0;
         table.addCell("Teleop High Attempted");
         for (int i = 0; i < matches.length; i++) {
-            table.addCell(matches[i].get);
+            table.addCell(matches[i].getShotsAttemptedHigh() + "");
         }
+        table.addCell(total/matches.length + "");
         
+        total = 0;
         table.addCell("Teleop High Scored");
         for (int i = 0; i < matches.length; i++) {
-            table.addCell(matches[i].get);
+            table.addCell(matches[i].getShotsScoredHigh() + "");
         }
-        
+        table.addCell(total/matches.length + "");
+
+        total = 0;
         table.addCell("Teleop Pyramid Attempted");
         for (int i = 0; i < matches.length; i++) {
-            table.addCell(matches[i].get);
+            table.addCell(matches[i].getShotsAttemptedPyramid() + "");
         }
+        table.addCell(total/matches.length + "");
         
+        total = 0;
         table.addCell("Teleop Pyramid Scored");
         for (int i = 0; i < matches.length; i++) {
-            table.addCell(matches[i].get);
+            table.addCell(matches[i].getShotsScoredPyramid() + "");
         }
-        
+        table.addCell(total/matches.length + "");
+
+        total = 0;
         table.addCell("Defense Rating");
         for (int i = 0; i < matches.length; i++) {
-            table.addCell(matches[i].get);
+            table.addCell(matches[i].getDefense() + "");
         }
-        
+        table.addCell(total/matches.length + "");
+
+        total = 0;
         table.addCell("Hang Level");
         for (int i = 0; i < matches.length; i++) {
-            table.addCell(matches[i].get);
+            table.addCell(matches[i].getHangLevel() + "");
         }
-        
+        table.addCell(total/matches.length + "");
+
+        total = 0;
         table.addCell("Hang Time");
         for (int i = 0; i < matches.length; i++) {
-            table.addCell(matches[i].get);
+            table.addCell(matches[i].getHangTime() + "");
         }
-        
-        Paragraph paragraph = new Paragraph(matches.teamName, catFont);
+        table.addCell(total/matches.length + "");
+
+        Paragraph paragraph = new Paragraph(matches[0].getTeamNum(), catFont);
         paragraph.setAlignment(Element.ALIGN_CENTER);
         content.add(paragraph);
         addEmptyLine(content, 1);
@@ -334,7 +380,6 @@ public class HelloPDF {
         document.add(content);
         document.close();
     }
-    */
 
     private static void addEmptyLine(Paragraph paragraph, int number) {
         for (int i = 0; i < number; i++) {
--- trunk/Scouting/KSI/src/ksi/MatchSheet.java
+++ trunk/Scouting/KSI/src/ksi/MatchSheet.java
@@ -9,9 +9,12 @@ package ksi;
  * @author Warfa
  */
 public class MatchSheet {
-    
+
+   
+    String teamNum;
     String startingPos;
-    double pointsScored;
+    int matchNum;
+    double autonPointsScored;
     double shotsAttemptedPyramid;
     double shotsAttemptedHigh;
     double shotsAttemptedMiddle;
@@ -19,5 +22,146 @@ public class MatchSheet {
     double shotsScoredPyramid;
     double shotsScoredHigh;
     double shotsScoredMiddle;
-            
+    double shotsScoredLow;
+    double defense;
+    double hangLevel;
+    double hangTime;
+
+    MatchSheet(int matchNum) {
+        this.matchNum = matchNum;
+        teamNum = "";
+        startingPos = "";
+        autonPointsScored = 0;
+        shotsAttemptedPyramid = 0;
+        shotsAttemptedHigh = 0;
+        shotsAttemptedMiddle = 0;
+        shotsAttemptedLow = 0;
+        shotsScoredPyramid = 0;
+        shotsScoredHigh = 0;
+        shotsScoredMiddle = 0;
+        shotsScoredLow = 0;
+        defense = 0;
+        hangLevel = 0;
+        hangTime = 0;
+    }
+     public String getTeamNum() {
+        return teamNum;
+    }
+
+    public void setTeamNum(String teamNum) {
+        this.teamNum = teamNum;
+    }
+    
+    
+    public void setStartingPos(String startingPos) {
+        this.startingPos = startingPos;
+    }
+
+    public void setMatchNum(int matchNum) {
+        this.matchNum = matchNum;
+    }
+
+    public void setAutonPointsScored(double autonPointsScored) {
+        this.autonPointsScored = autonPointsScored;
+    }
+
+    public void setShotsAttemptedPyramid(double shotsAttemptedPyramid) {
+        this.shotsAttemptedPyramid = shotsAttemptedPyramid;
+    }
+
+    public void setShotsAttemptedHigh(double shotsAttemptedHigh) {
+        this.shotsAttemptedHigh = shotsAttemptedHigh;
+    }
+
+    public void setShotsAttemptedMiddle(double shotsAttemptedMiddle) {
+        this.shotsAttemptedMiddle = shotsAttemptedMiddle;
+    }
+
+    public void setShotsAttemptedLow(double shotsAttemptedLow) {
+        this.shotsAttemptedLow = shotsAttemptedLow;
+    }
+
+    public void setShotsScoredPyramid(double shotsScoredPyramid) {
+        this.shotsScoredPyramid = shotsScoredPyramid;
+    }
+
+    public void setShotsScoredHigh(double shotsScoredHigh) {
+        this.shotsScoredHigh = shotsScoredHigh;
+    }
+
+    public void setShotsScoredMiddle(double shotsScoredMiddle) {
+        this.shotsScoredMiddle = shotsScoredMiddle;
+    }
+
+    public void setShotsScoredLow(double shotsScoredLow) {
+        this.shotsScoredLow = shotsScoredLow;
+    }
+
+    public void setDefense(double defense) {
+        this.defense = defense;
+    }
+
+    public void setHangLevel(double hangLevel) {
+        this.hangLevel = hangLevel;
+    }
+
+    public void setHangTime(double hangTime) {
+        this.hangTime = hangTime;
+    }
+
+    public String getStartingPos() {
+        return startingPos;
+    }
+
+    public int getMatchNum() {
+        return matchNum;
+    }
+
+    public double getAutonPointsScored() {
+        return autonPointsScored;
+    }
+
+    public double getShotsAttemptedPyramid() {
+        return shotsAttemptedPyramid;
+    }
+
+    public double getShotsAttemptedHigh() {
+        return shotsAttemptedHigh;
+    }
+
+    public double getShotsAttemptedMiddle() {
+        return shotsAttemptedMiddle;
+    }
+
+    public double getShotsAttemptedLow() {
+        return shotsAttemptedLow;
+    }
+
+    public double getShotsScoredPyramid() {
+        return shotsScoredPyramid;
+    }
+
+    public double getShotsScoredHigh() {
+        return shotsScoredHigh;
+    }
+
+    public double getShotsScoredMiddle() {
+        return shotsScoredMiddle;
+    }
+
+    public double getShotsScoredLow() {
+        return shotsScoredLow;
+    }
+
+    public double getDefense() {
+        return defense;
+    }
+
+    public double getHangLevel() {
+        return hangLevel;
+    }
+
+    public double getHangTime() {
+        return hangTime;
+    }
 }
