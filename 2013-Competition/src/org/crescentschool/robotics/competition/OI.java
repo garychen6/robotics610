7e93245baf0919ba2586748de0849e309e6ebc6e
warfajibril <warfajibril@crescentschool.org>
2013-01-08 21:56:39 +0000

    It Begins

---
+++ 2013-Competition/src/org/crescentschool/robotics/competition/OI.java
@@ -0,0 +1,14 @@
+
+package org.crescentschool.robotics.competition;
+
+import edu.wpi.first.wpilibj.buttons.Button;
+import edu.wpi.first.wpilibj.buttons.DigitalIOButton;
+
+/**
+ * This class is the glue that binds the controls on the physical operator
+ * interface to the commands and command groups that allow control of the robot.
+ */
+public class OI {
+   
+}
+
