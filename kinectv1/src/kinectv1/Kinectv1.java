a45b2a7e0231449971dc09c01a7cd36e8f06f84f
scottso <scottso@crescentschool.org>
2013-01-08 22:44:13 +0000

    V1 for Kinect Tracking... not the best

---
+++ kinectv1/src/kinectv1/Kinectv1.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kinectv1;
+
+/**
+ *
+ * @author scott
+ */
+public class Kinectv1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
