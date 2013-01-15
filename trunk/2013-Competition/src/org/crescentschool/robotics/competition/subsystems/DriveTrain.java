f0fae754dab0e884c38fb456e016b3804c023615
ianlo@crescentschool.org <ianlo@crescentschool.org@52f67a3d-da4c-c82c-9fa6-35572dec3218>
2013-01-15 23:16:09 +0000

    OtherShooter

--- trunk/2013-Competition/src/org/crescentschool/robotics/competition/subsystems/DriveTrain.java
+++ trunk/2013-Competition/src/org/crescentschool/robotics/competition/subsystems/DriveTrain.java
@@ -1,18 +1,40 @@
-
 package org.crescentschool.robotics.competition.subsystems;
 
+import edu.wpi.first.wpilibj.CANJaguar;
+import edu.wpi.first.wpilibj.Victor;
+import edu.wpi.first.wpilibj.can.CANTimeoutException;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import org.crescentschool.robotics.competition.commands.KajDrive;
+import org.crescentschool.robotics.competition.constants.ElectricalConstants;
 
 /**
  *
  */
 public class DriveTrain extends Subsystem {
-    // Put methods for controlling this subsystem
-    // here. Call these from Commands.
+
+    CANJaguar jagRightMaster;
+    Victor vicRightSlave;
+    Victor vicRightSlave2;
+    CANJaguar jagLeftMaster;
+    Victor vicLeftSlave;
+    Victor vicLeftSlave2;
 
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
-        //setDefaultCommand(new MySpecialCommand());
+        setDefaultCommand(new KajDrive());
+
+    }
+
+    DriveTrain() {
+        try {
+            jagRightMaster = new CANJaguar(2);
+            jagLeftMaster = new CANJaguar(3);
+            vicRightSlave = new Victor(4);
+            vicRightSlave2 = new Victor(5);
+            vicLeftSlave = new Victor(6);
+            vicLeftSlave2 = new Victor (7);
+        } catch (CANTimeoutException ex) {
+            ex.printStackTrace();
+        }
     }
 }
-
