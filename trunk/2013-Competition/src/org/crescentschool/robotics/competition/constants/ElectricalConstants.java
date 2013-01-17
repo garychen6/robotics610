f338d816a1fdaf1dd35524d23256fb99b5c2bfd0
jeffreyseto@crescentschool.org <jeffreyseto@crescentschool.org@52f67a3d-da4c-c82c-9fa6-35572dec3218>
2013-01-17 17:12:04 +0000

    Misnamed PWM as CAN in electrical constants fixed.
    Slave master Ian does not give enough food.

--- trunk/2013-Competition/src/org/crescentschool/robotics/competition/constants/ElectricalConstants.java
+++ trunk/2013-Competition/src/org/crescentschool/robotics/competition/constants/ElectricalConstants.java
@@ -9,5 +9,16 @@ package org.crescentschool.robotics.competition.constants;
  * @author Warfa
  */
 public class ElectricalConstants {
-    
+    //CAN ID of the right drivetrain Jaguar
+    final public static int DriveRightMaster = 2;
+    //CAN ID of the left drivetrain Jaguar
+    final public static int DriveLeftMaster = 3;
+    //PWM ID of the first right drivetrain Victor
+    final public static int DriveRightSlave1 = 4;
+    //PWM ID of the second right drivetrain Victor
+    final public static int DriveRightSlave2 = 5;
+    //PWM ID of the first left drivetrain Victor
+    final public static int DriveLeftSlave1 = 6;
+    //PWM ID of the second left drivetrain Victor
+    final public static int DriveLeftSlave2 = 7;
 }
