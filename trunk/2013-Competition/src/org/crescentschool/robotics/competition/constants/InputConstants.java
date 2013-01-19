b586047fef7f27d9ba31026bad77592f24503840
jeffreyseto@crescentschool.org <jeffreyseto@crescentschool.org@52f67a3d-da4c-c82c-9fa6-35572dec3218>
2013-01-19 20:14:00 +0000

    Input constants finished. YAY :)

--- trunk/2013-Competition/src/org/crescentschool/robotics/competition/constants/InputConstants.java
+++ trunk/2013-Competition/src/org/crescentschool/robotics/competition/constants/InputConstants.java
@@ -9,6 +9,8 @@ package org.crescentschool.robotics.competition.constants;
  * @author jeffrey
  */
 public class InputConstants {
+    //Thumbsticks:
+    
     //Left thumbstick X-axis
     final public static int leftXAxis = 1;
     //Left thumbstick Y-axis
@@ -17,4 +19,16 @@ public class InputConstants {
     final public static int rightXAxis = 3;
     //Right thumbstick Y-axis
     final public static int rightYAxis = 4;
+    
+    //Buttons:
+    final public static int aButton = 2;
+    final public static int bButton = 3;
+    final public static int xButton = 1;
+    final public static int yButton = 4;
+    final public static int rightBumper = 6;
+    final public static int leftBumper = 5;
+    final public static int rightTrigger = 8;
+    final public static int leftTrigger = 7;
+    final public static int startButton = 10;
+    final public static int selectButton = 9;
 }
