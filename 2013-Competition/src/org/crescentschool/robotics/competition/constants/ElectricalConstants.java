be7908bdead5501a70b4822595e64a4fc91d68a0
jeffreyseto <jeffreyseto@crescentschool.org>
2013-01-19 16:19:20 +0000

    Slave master Ian does not give enough food.

--- 2013-Competition/src/org/crescentschool/robotics/competition/constants/ElectricalConstants.java
+++ 2013-Competition/src/org/crescentschool/robotics/competition/constants/ElectricalConstants.java
@@ -10,15 +10,19 @@ package org.crescentschool.robotics.competition.constants;
  */
 public class ElectricalConstants {
     //CAN ID of the right drivetrain Jaguar
-    final public static int DriveRightMaster = 2;
+    final public static int jagRightMaster = 2;
     //CAN ID of the left drivetrain Jaguar
-    final public static int DriveLeftMaster = 3;
+    final public static int jagLeftMaster = 3;
     //PWM ID of the first right drivetrain Victor
-    final public static int DriveRightSlave1 = 4;
+    final public static int victorRightSlaveMid = 4;
     //PWM ID of the second right drivetrain Victor
-    final public static int DriveRightSlave2 = 5;
+    final public static int victorRightSlaveBack = 5;
     //PWM ID of the first left drivetrain Victor
-    final public static int DriveLeftSlave1 = 6;
+    final public static int victorLeftSlaveMid = 6;
     //PWM ID of the second left drivetrain Victor
-    final public static int DriveLeftSlave2 = 7;
+    final public static int victorLeftSlaveBack = 7;
+    
+    
+    //CAN ID of the shooter Jaguar
+    final public static int jagShooter = 7;
 }
