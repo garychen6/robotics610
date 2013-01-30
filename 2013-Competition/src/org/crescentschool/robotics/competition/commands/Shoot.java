175b6f447860e9c2f45ceeab69f403cf3bb781c1
warfajibril <warfajibril@crescentschool.org>
2013-01-30 23:23:49 +0000

    Here are my constants. Call Me maybe

--- 2013-Competition/src/org/crescentschool/robotics/competition/commands/Shoot.java
+++ 2013-Competition/src/org/crescentschool/robotics/competition/commands/Shoot.java
@@ -27,6 +27,7 @@ public class Shoot extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+        //ADD TRIM
     }
 
     // Make this return true when this Command no longer needs to run execute()
