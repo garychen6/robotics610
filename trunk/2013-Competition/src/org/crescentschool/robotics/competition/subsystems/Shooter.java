f740cd26cb5db64b50066125f51a0197b3c2b6f5
warfajibril@crescentschool.org <warfajibril@crescentschool.org@52f67a3d-da4c-c82c-9fa6-35572dec3218>
2013-02-07 22:49:29 +0000

    Bass Drop

--- trunk/2013-Competition/src/org/crescentschool/robotics/competition/subsystems/Shooter.java
+++ trunk/2013-Competition/src/org/crescentschool/robotics/competition/subsystems/Shooter.java
@@ -6,6 +6,7 @@ package org.crescentschool.robotics.competition.subsystems;
 
 import edu.wpi.first.wpilibj.CANJaguar;
 import edu.wpi.first.wpilibj.GearTooth;
+import edu.wpi.first.wpilibj.PIDController;
 import edu.wpi.first.wpilibj.can.CANTimeoutException;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import org.crescentschool.robotics.competition.commands.ShooterPIDCommand;
@@ -19,7 +20,6 @@ public class Shooter extends Subsystem {
 
     static Shooter instance = null;
     CANJaguar shooter;
-    ShooterPIDCommand pidController;
     GearTooth gearTooth;
 
     public static Shooter getInstance() {
@@ -31,20 +31,17 @@ public class Shooter extends Subsystem {
 
     Shooter() {
         try {
-            gearTooth = new GearTooth(2);
-            gearTooth.setMaxPeriod(5);
-            gearTooth.setSemiPeriodMode(true);
-            gearTooth.start();
-            
-            shooter = new CANJaguar(ElectricalConstants.jagShooter);
+            shooter = new CANJaguar(7);
             shooter.changeControlMode(CANJaguar.ControlMode.kSpeed);
             shooter.setSpeedReference(CANJaguar.SpeedReference.kQuadEncoder);
             shooter.configEncoderCodesPerRev(8);
             shooter.changeControlMode(CANJaguar.ControlMode.kVoltage);
             shooter.configNeutralMode(CANJaguar.NeutralMode.kCoast);
             shooter.enableControl(0);
-            
-            
+            gearTooth = new GearTooth(2);
+            gearTooth.setMaxPeriod(5);
+            gearTooth.setSemiPeriodMode(true);
+            gearTooth.start();
         } catch (CANTimeoutException ex) {
             ex.printStackTrace();
         }
