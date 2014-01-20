8e2ccdb6d9538889827b0c60068a3038a2d5051b
ianlo <ianlo@crescentschool.org>
2014-01-20 23:12:50 +0000

    Cam shooter code

---
+++ 2014 - Cam Shooter/src/edu/wpi/first/wpilibj/templates/InputConstants.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.wpi.first.wpilibj.templates;
+
+/**
+ *
+ * @author charlesju
+ */
+public class InputConstants {
+    
+    final public static int r2button = 8;
+    final public static int l2button = 8;
+    
+    
+}
---
+++ 2014 - Cam Shooter/src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
@@ -0,0 +1,88 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) FIRST 2008. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+package edu.wpi.first.wpilibj.templates;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.Victor;
+
+/**
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to each mode, as described in the IterativeRobot
+ * documentation. If you change the name of this class or the package after
+ * creating this project, you must also update the manifest file in the resource
+ * directory.
+ */
+public class RobotTemplate extends IterativeRobot {
+
+    int cameraRelay = 1;
+    Victor catapult;
+    Joystick joystick;
+    DigitalInput optical;
+    boolean loaded = false;
+    boolean rest = true;
+    int stopping = 0;
+
+   
+    /**
+     * This function is run when the robot is first started up and should be
+     * used for any initialization code.
+     */
+    public void robotInit() {
+
+        catapult = new Victor(2);
+        joystick = new Joystick(1);
+        optical = new DigitalInput(1, cameraRelay);
+    }
+
+    /**
+     * This function is called periodically during autonomous
+     */
+    public void autonomousPeriodic() {
+    }
+
+    /**
+     * This function is called periodically during operator control
+     */
+    public void teleopPeriodic() {
+        if (optical.get() && rest == false) {
+            catapult.set(1);
+
+            stopping = 10;
+        } else if (joystick.getRawButton(1)) {
+            catapult.set(1);
+            System.out.println("Trigger pressed");
+            rest = false;
+        } else {
+            if (stopping > 0) {
+                rest = true;
+                catapult.set(-0.2);
+                stopping--;
+                System.out.println("Stopping");
+
+            } else {
+
+                catapult.set(0);
+
+                System.out.println("Stopped");
+
+            }
+
+
+        }
+
+
+
+    }
+
+    /**
+     * This function is called periodically during test mode
+     */
+    public void testPeriodic() {
+    }
+}
